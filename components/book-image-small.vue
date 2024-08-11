530988a31ac6d133b18db72f5e957ff7c8d7871f
Jessica Aissa <jessica.aissa@live.com>
2024-08-11 13:28:58 +0200

    fix: 🐛 inset book image border

--- components/book-image-small.vue
+++ components/book-image-small.vue
@@ -8,8 +8,8 @@
       v-if="!coverSrc"
       class="!h-48 !rounded-2xl !p-5"
       :class="{
-        'hover:border hover:border-accent-dark hover:shadow-md': !selectable,
-        '!border-2 !border-main': selectable && _selected,
+        'hover:shadow-md hover:ring hover:ring-accent-dark': !selectable,
+        '!ring-2 !ring-main': selectable && _selected,
       }"
     />
     <NuxtImg
@@ -18,8 +18,8 @@
       :alt="alt"
       class="h-auto w-full max-w-full rounded-2xl object-cover object-center md:h-48 md:w-auto"
       :class="{
-        'hover:border hover:border-accent-dark hover:shadow-md': !selectable,
-        'border-2 border-main': selectable && _selected,
+        'hover:shadow-md hover:ring hover:ring-accent-dark': !selectable,
+        'ring-2 ring-main': selectable && _selected,
       }"
     />
   </NuxtLink>
