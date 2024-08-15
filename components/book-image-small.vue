1eb76b3ec0e3cc3bc9eb31328eb0f37a29a5e00e
Jessica Aissa <jessica.aissa@live.com>
2024-08-15 10:01:22 +0200

    fix: 🐛 book image small ring

--- components/book-image-small.vue
+++ components/book-image-small.vue
@@ -8,7 +8,7 @@
       v-if="!coverSrc"
       class="!h-48 !rounded-2xl !p-5"
       :class="{
-        'hover:shadow-md hover:ring hover:ring-accent-dark': !selectable,
+        'hover:ring-1 hover:ring-accent-dark': !selectable,
         '!ring-2 !ring-main': selectable && selected,
       }"
     />
@@ -18,8 +18,8 @@
       :alt="alt"
       class="h-auto w-full max-w-full rounded-2xl object-cover object-center md:h-48 md:w-auto"
       :class="{
-        'hover:shadow-md hover:ring hover:ring-accent-dark': !selectable,
-        'ring-2 ring-main': selectable && selected,
+        'hover:ring-1 hover:ring-accent-dark': !selectable,
+        '!ring-2 ring-main': selectable && selected,
       }"
     />
   </NuxtLink>
