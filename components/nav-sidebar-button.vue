dccef8331f64f71744bf857ac1cb352116f5bf89
Jessica Aissa <jessica.aissa@live.com>
2024-09-02 18:31:31 +0200

    feat: 🎸 redesign nav sidebar

--- components/nav-sidebar-button.vue
+++ components/nav-sidebar-button.vue
@@ -1,9 +1,9 @@
 <template>
   <NuxtLink
     :to="to"
-    class="flex cursor-pointer items-center gap-3 rounded-m px-5 py-3 text-lg transition-all hover:bg-accent/30 active:bg-accent-dark/20"
+    class="flex cursor-pointer items-center gap-3 rounded-lg px-5 py-3 text-base transition-all hover:bg-accent/30 active:bg-accent-dark/20"
     :class="{
-      'bg-accent/20 hover:bg-accent/20': active,
+      'bg-main text-white hover:bg-main/80': active,
     }"
   >
     <slot name="icon" :size="ICON_SIZE_MEDIUM" stroke="1.5" />
---
+++ components/nav-sidebar.story.vue
@@ -0,0 +1,11 @@
+<script setup>
+import NavSidebar from './nav-sidebar.vue'
+</script>
+
+<template>
+  <Story title="Navigation Sidebar">
+    <div class="h-screen">
+      <NavSidebar />
+    </div>
+  </Story>
+</template>
