fb71b0eeb64e7d49062391f9ecb113b46f4693b4
Jessica Aissa <jessica.aissa@live.com>
2024-09-02 19:06:51 +0200

    feat: 🎸 redesign checkbox and list checkbox

---
+++ components/checkbox.story.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Checkbox from './checkbox.vue'
+</script>
+
+<template>
+  <Story title="Checkbox">
+    <Checkbox>Checkbox</Checkbox>
+  </Story>
+</template>
---
+++ components/list-checkbox.story.vue
@@ -0,0 +1,15 @@
+<script setup>
+import ListCheckbox from './list-checkbox.vue'
+import ListCheckboxOption from './list-checkbox-option.vue'
+</script>
+
+<template>
+  <Story title="List Checkbox">
+    <ListCheckbox>
+      <ListCheckboxOption value="item1">Item 1</ListCheckboxOption>
+      <ListCheckboxOption value="item2">Item 2</ListCheckboxOption>
+      <ListCheckboxOption value="item3">Item 3</ListCheckboxOption>
+      <ListCheckboxOption value="item4">Item 4</ListCheckboxOption>
+    </ListCheckbox>
+  </Story>
+</template>
