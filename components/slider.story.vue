706f5e26c78d850beb044e1af78546678c5d91b4
Jessica Aissa <jessica.aissa@live.com>
2024-09-02 18:06:07 +0200

    feat: 🎸 redesign slider

---
+++ components/slider.story.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Slider from './slider.vue'
+
+const state = reactive({
+  label: 'Slider',
+  min: 0,
+  max: 20,
+  minValue: 3,
+  maxValue: 18,
+  step: 1,
+})
+</script>
+
+<template>
+  <Story title="Slider"><Slider v-bind="state" /></Story>
+</template>
