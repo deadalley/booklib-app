03ed337acccb08d7235a6f2d01dd0d1f41e86e54
Jessica Aissa <jessica.aissa@live.com>
2024-09-02 17:55:15 +0200

    feat: 🎸 redesign input

---
+++ components/input.story.vue
@@ -0,0 +1,37 @@
+<script setup>
+import config from '../formkit.config.ts'
+import Input from './input.vue'
+
+const state = reactive({
+  name: 'inputName',
+  label: 'Label',
+  placeholder: 'Placeholder',
+  editing: true,
+})
+
+function setupApp({ app, story, variant }) {
+  app.use(plugin, defaultConfig(config))
+}
+</script>
+
+<template>
+  <Story title="Input">
+    <Variant title="Default">
+      <Input
+        :name="state.name"
+        :label="state.label"
+        :placeholder="state.placeholder"
+      />
+    </Variant>
+    <Variant title="Prefix">
+      <Input>
+        <template #prefix>{{ state.prefix }}</template>
+      </Input>
+    </Variant>
+    <Variant title="Postfix">
+      <Input>
+        <template #postfix>{{ state.postfix }}</template>
+      </Input>
+    </Variant>
+  </Story>
+</template>
--- components/input.vue
+++ components/input.vue
@@ -2,7 +2,7 @@
   <div v-if="!editing" class="formkit-wrapper flex-1">
     <label class="formkit-label">{{ $attrs.label }}</label>
     <h5 v-if="inputModel">{{ displayValue }}</h5>
-    <p v-if="!inputModel" class="text-gray-dark">Empty</p>
+    <p v-if="!inputModel" class="text-black">Empty</p>
   </div>
   <FormKit
     v-if="editing"
