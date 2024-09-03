769b66a630ae68eb922af94aecf6ea1b29e227bf
Jessica Aissa <jessica.aissa@live.com>
2024-09-03 22:43:22 +0200

    fix: 🐛 reduce padding in pill

--- components/pill.vue
+++ components/pill.vue
@@ -1,6 +1,6 @@
 <template>
   <span
-    class="relative inline-flex cursor-pointer items-center gap-2 rounded-lg border px-12 py-2 font-medium transition-all active:bg-accent"
+    class="relative inline-flex cursor-pointer items-center gap-2 rounded-lg border px-8 py-2 font-medium transition-all active:bg-accent"
     :class="{
       'border-accent bg-accent-light hover:bg-accent-light/20': !selected,
       'border-accent-dark bg-accent hover:bg-accent/80': !!selected,
