79f3e8cb912872c02d1e17153ec0b1f9faa77c26
Jessica Aissa <jessica.aissa@live.com>
2024-09-03 21:37:23 +0200

    feat: 🎸 move expanded collection to radix

--- utils/factories.ts
+++ utils/factories.ts
@@ -1,5 +1,6 @@
 import { faker } from '@faker-js/faker'
 import type { Book } from '~/types/book'
+import type { Collection } from '~/types/collection'
 
 export function buildBook(overwrite?: Partial<Book>): Book {
   return {
@@ -17,6 +18,16 @@ export function buildBook(overwrite?: Partial<Book>): Book {
     summary: faker.lorem.lines(5),
     year: faker.date.past().getFullYear(),
     genres: faker.helpers.multiple(faker.color.human, { count: 5 }),
+    ...overwrite,
+  }
+}
+
+export function buildCollection(overwrite?: Partial<Collection>): Collection {
+  return {
+    id: faker.number.float(),
+    name: faker.commerce.productName(),
+    books: [1, 2, 3],
+    createdAt: faker.date.past().toISOString(),
     ...overwrite,
   }
 }
