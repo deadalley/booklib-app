0123c99d0029f6d6874c3431bbca4f5204048401
Jessica Aissa <jessica.aissa@live.com>
2024-09-03 13:51:44 +0200

    feat: 🎸 update design of view switch

--- components/switch-option.vue
+++ components/switch-option.vue
@@ -4,12 +4,12 @@
     v-slot="{ checked }"
     as="li"
     :value="value"
-    class="radio-group-option flex cursor-pointer items-center border-r border-r-accent bg-accent-light text-black ring-inset first:rounded-l-lg last:rounded-r-lg last:border-0 hover:bg-white hover:ring-1 hover:ring-accent-dark active:bg-accent lg:w-full"
+    class="radio-group-option flex cursor-pointer items-center border border-accent bg-white text-black ring-inset first:rounded-l-lg last:rounded-r-lg hover:bg-accent active:bg-accent-dark lg:w-full"
   >
     <span
       class="flex items-center rounded-[inherit] p-3 transition-all lg:w-full"
       :class="{
-        'bg-accent-dark': !!checked,
+        'bg-accent-light hover:bg-accent': !!checked,
       }"
     >
       <slot name="icon" :size="21" stroke="1.5" />
