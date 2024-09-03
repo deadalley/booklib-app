d7c4989aa087b8bda9c7a5bb78d9ee874ab1165e
Jessica Aissa <jessica.aissa@live.com>
2024-09-03 13:46:52 +0200

    feat: 🎸 animate expanded collection

--- components/expanded-collection.vue
+++ components/expanded-collection.vue
@@ -4,22 +4,33 @@
       class="mb-2 flex items-center justify-between rounded-lg px-4 py-2 text-start hover:bg-accent-light"
     >
       <div class="flex items-baseline gap-3 lg:flex-col xl:flex-row">
-        <h4>
-          {{ collection.name }}
-        </h4>
+        <NuxtLink :to="`/library/collections/${collection.id}`">
+          <h4>
+            {{ collection.name }}
+          </h4>
+        </NuxtLink>
         <h6 class="flex gap-3 text-accent-dark">TOTAL {{ books.length }}</h6>
       </div>
       <IconChevronDown v-if="!open" :size="20" />
       <IconChevronUp v-if="open" :size="20" />
     </DisclosureButton>
-    <DisclosurePanel class="flex gap-x-6 overflow-x-auto p-1">
-      <bl-book-card
-        v-for="book in books"
-        :key="book.title"
-        :book="book"
-        class="w-1/12"
-      />
-    </DisclosurePanel>
+    <transition
+      enter-active-class="transition duration-100 ease-out"
+      enter-from-class="transform opacity-0"
+      enter-to-class="transform opacity-100"
+      leave-active-class="transition duration-100 ease-out"
+      leave-from-class="transform opacity-100"
+      leave-to-class="transform opacity-0"
+    >
+      <DisclosurePanel class="flex gap-x-6 overflow-x-auto p-4">
+        <bl-book-card
+          v-for="book in books"
+          :key="book.title"
+          :book="book"
+          class="w-1/12"
+        />
+      </DisclosurePanel>
+    </transition>
   </Disclosure>
 </template>
 
