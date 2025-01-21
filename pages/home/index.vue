0df37917e177ebf1a15aaf695ac59aa2c566c697
Jessica Aissa <jessica.aissa@live.com>
2025-01-21 01:11:00 +0100

    feat: 🎸 display empty library prompt

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -1,17 +1,27 @@
 <template>
   <NuxtLayout name="dashboard" title="Home">
-    <div class="flex flex-col">
+    <div v-if="isEmpty" class="flex flex-col">
       <h1 class="mb-8">
         Welcome to your new library, {{ user?.user_metadata.name }}!
       </h1>
-      <div class="flex items-center gap-5">
-        <p class="text-xl">Start by adding your first book:</p>
-        <NuxtLink class="flex md:inline-flex" to="/library/books/new">
+      <div class="flex flex-col items-center gap-4">
+        <NuxtImg
+          src="/book-graphics.svg"
+          alt="Book"
+          class="h-96 object-cover object-center"
+        />
+        <h4>Your library is empty.</h4>
+        <p class="text-center text-lg">
+          It looks like you don't have any books yet in your library.
+          <br />
+          Let's add your first book.
+        </p>
+        <NuxtLink class="mt-4 flex md:inline-flex" to="/library/books/new">
           <bl-button expand>
             <template #prependIcon="prependIcon">
               <IconPlus v-bind="prependIcon" />
             </template>
-            My first book
+            Add first book to library
           </bl-button>
         </NuxtLink>
       </div>
@@ -22,6 +32,8 @@
 <script setup lang="ts">
 import { IconPlus } from '@tabler/icons-vue'
 const user = useSupabaseUser()
+
+const { data: isEmpty } = await useFetch<number>('/api/library/is-empty')
 
 useHead({
   title: 'BookLib | Home',
---
+++ server/api/library/is-empty.get.ts
@@ -0,0 +1,17 @@
+import { serverSupabaseClient, serverSupabaseUser } from '#supabase/server'
+import type { Database } from '~/types/db.generate'
+
+export default defineEventHandler(async (event) => {
+  const user = await serverSupabaseUser(event)
+  const client = await serverSupabaseClient<Database>(event)
+
+  if (!user?.id) {
+    throw createError('Unauthenticated')
+  } else {
+    const { count } = await client
+      .from('books')
+      .select('*', { count: 'exact', head: true })
+
+    return count === 0 || count === null || count === undefined
+  }
+})
