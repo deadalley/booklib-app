1b6612ae3b654ee43c8c3053b13c55b0f2b657e3
Jessica Aissa <jessica.aissa@live.com>
2025-05-23 16:29:46 +0200

    feat: 🎸 post collection from db client

--- server/api/collections/index.post.ts
+++ server/api/collections/index.post.ts
@@ -1,81 +1,11 @@
-import { serverSupabaseClient, serverSupabaseUser } from '#supabase/server'
-import { insertBooksInCollection } from '~/server/utils/insert-books-in-collection'
+import { db } from '~/services/db.service'
 import type { Collection } from '~/types/collection'
-import type { Database } from '~/types/db.generate'
-import {
-  collectionToDbCollection,
-  dbCollectionToCollection,
-  logger,
-} from '~/utils'
+import { dbCollectionToCollection } from '~/utils'
 
 export default defineEventHandler(async (event) => {
-  const user = await serverSupabaseUser(event)
-  const client = await serverSupabaseClient<Database>(event)
-
   const collection: Collection = await readBody(event)
 
-  if (!user?.id) {
-    throw createError('Unauthenticated')
-  } else {
-    const { data, error } = await client
-      .from('collections')
-      .upsert([collectionToDbCollection(collection, user.id)], {
-        onConflict: 'id',
-        defaultToNull: true,
-      }).select(`
-        *,
-        "collection-book" (
-          order,
-          book_id
-        )
-      `)
+  const data = await db.createCollection(event, collection)
 
-    if (error) {
-      logger.error(error)
-      throw createError({ statusMessage: error.message })
-    }
-
-    const collectionId = data?.[0].id ?? collection.id
-
-    const { error: deleteBookCollectionError } = await client
-      .from('collection-book')
-      .delete()
-      .eq('user_id', user.id)
-      .eq('collection_id', collectionId)
-
-    if (deleteBookCollectionError) {
-      logger.error(deleteBookCollectionError)
-      throw createError({ statusMessage: deleteBookCollectionError.message })
-    }
-
-    const { error: bookCollectionError } = await insertBooksInCollection(
-      client,
-      user.id,
-      collectionId,
-      collection.books,
-    )
-
-    if (bookCollectionError) {
-      logger.error(bookCollectionError)
-
-      // revert the changes manually until Supabase supports transactions
-      const originalCollection = data.find(({ id }) => id === collectionId)
-
-      if (originalCollection) {
-        await insertBooksInCollection(
-          client,
-          user.id,
-          collectionId,
-          originalCollection['collection-book'].map(({ book_id, order }) => ({
-            id: book_id,
-            order,
-          })),
-        )
-      }
-
-      throw createError({ statusMessage: bookCollectionError.message })
-    }
-
-    return dbCollectionToCollection(data[0], data[0]['collection-book'])
-  }
+  return data && dbCollectionToCollection(data, data['collection-book'])
 })
--- services/supabase.service.ts
+++ services/supabase.service.ts
@@ -3,8 +3,14 @@ import type { GetBooksQuerySearchParams } from '~/types/api'
 import type { Database } from '~/types/db.generate'
 import type { BookDB, CollectionDB } from '~/types/database'
 import type { H3Event, EventHandlerRequest } from 'h3'
-import { bookToDbBook, executePromisesInChunks, logger } from '../utils'
+import {
+  bookToDbBook,
+  collectionToDbCollection,
+  executePromisesInChunks,
+  logger,
+} from '../utils'
 import type { Book } from '~/types/book'
+import type { Collection } from '~/types/collection'
 
 async function authenticate(event: H3Event<EventHandlerRequest>) {
   const user = await serverSupabaseUser(event)
@@ -120,7 +126,7 @@ export async function createBook(
 
   if (deleteBookCollectionError) {
     logger.error(deleteBookCollectionError)
-    throw createError({ statusMessage: deleteBookCollectionError.message })
+    throw createError(deleteBookCollectionError)
   }
 
   const { error: bookCollectionError } = await client
@@ -215,4 +221,97 @@ export async function getCollections(
   }
 
   return data ?? []
+}
+
+export async function createCollection(
+  event: H3Event<EventHandlerRequest>,
+  collection: Collection,
+): Promise<
+  | (CollectionDB & {
+      'collection-book': { book_id: Book['id']; order: number }[]
+    })
+  | null
+> {
+  const { user, client } = await authenticate(event)
+
+  const { data, error } = await client
+    .from('collections')
+    .upsert([collectionToDbCollection(collection, user.id)], {
+      onConflict: 'id',
+      defaultToNull: true,
+    }).select(`
+        *,
+        "collection-book" (
+          order,
+          book_id
+        )
+      `)
+
+  if (error) {
+    logger.error(error)
+    throw createError(error)
+  }
+
+  const collectionId = data?.[0].id ?? collection.id
+
+  const { error: deleteBookCollectionError } = await client
+    .from('collection-book')
+    .delete()
+    .eq('user_id', user.id)
+    .eq('collection_id', collectionId)
+
+  if (deleteBookCollectionError) {
+    logger.error(deleteBookCollectionError)
+    throw createError(deleteBookCollectionError)
+  }
+
+  const { error: bookCollectionError } = await client
+    .from('collection-book')
+    .insert(
+      collection.books.map(({ id: bookId, order }) => ({
+        book_id: bookId,
+        collection_id: collectionId,
+        user_id: user.id,
+        order,
+      })),
+    )
+
+  if (bookCollectionError) {
+    logger.error(bookCollectionError)
+
+    // revert the changes manually until Supabase supports transactions
+    const originalCollection = data.find(({ id }) => id === collectionId)
+
+    if (originalCollection) {
+      await insertBooksInCollection(
+        client,
+        user.id,
+        collectionId,
+        originalCollection['collection-book'].map(({ book_id, order }) => ({
+          id: book_id,
+          order,
+        })),
+      )
+    }
+
+    throw createError(bookCollectionError)
+  }
+
+  return data[0]
+}
+
+export async function deleteCollection(
+  event: H3Event<EventHandlerRequest>,
+  id: BookDB['id'],
+): Promise<BookDB['id'] | null> {
+  const { client } = await authenticate(event)
+
+  const { error } = await client.from('books').delete().eq('id', +id)
+
+  if (error) {
+    logger.error(error)
+    throw createError(error)
+  }
+
+  return id
 }
