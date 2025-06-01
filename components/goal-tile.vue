0efbf42e70a175d8a9938f17855efbbdbbb126b9
Jessica Aissa <jessica.aissa@live.com>
2025-06-01 13:25:54 +0200

    feat: 🎸 wrap content in accordion

--- components/goal-tile.vue
+++ components/goal-tile.vue
@@ -1,27 +1,29 @@
 <template>
   <bl-tile v-if="goal" v-bind="$attrs">
-    <div class="mb-2 flex items-center gap-2">
-      <component
-        :is="icons[GOAL_TYPE_MAP[goal.type].icon]"
-        :class="getProgressColor(goal.status, 'text')"
-        :size="ICON_SIZE_LARGE"
-        stroke="1.5"
-      />
-      <h5>{{ goal.title }}</h5>
-      <span class="ml-2">
-        <bl-total-tag
-          :variant="goal.status === 'tracking' ? 'primary' : 'secondary'"
-        >
-          <IconConfetti
-            v-if="goal.status === 'finished'"
-            class="text-main"
-            :size="16"
-          />
-          {{ GOAL_STATUS_MAP[goal.status].description }}
-        </bl-total-tag>
-      </span>
-    </div>
-    <div class="flex flex-col gap-2">
+    <template #title>
+      <div class="flex items-center gap-2">
+        <component
+          :is="icons[GOAL_TYPE_MAP[goal.type].icon]"
+          :class="getProgressColor(goal.status, 'text')"
+          :size="ICON_SIZE_LARGE"
+          stroke="1.5"
+        />
+        <h5>{{ goal.title }}</h5>
+        <span class="ml-2">
+          <bl-total-tag
+            :variant="goal.status === 'tracking' ? 'primary' : 'secondary'"
+          >
+            <IconConfetti
+              v-if="goal.status === 'finished'"
+              class="text-main"
+              :size="16"
+            />
+            {{ GOAL_STATUS_MAP[goal.status].description }}
+          </bl-total-tag>
+        </span>
+      </div>
+    </template>
+    <div class="mt-8 flex flex-col gap-2">
       <bl-progress-bar
         v-model:progress-value="goal.progress"
         size="sm"
@@ -32,38 +34,42 @@
         <p class="text-accent-darker">{{ toFullDate(goal.finishAt) }}</p>
       </div>
     </div>
-    <bl-tabs default-value="progress" class="mt-4" full>
-      <template #options="options">
-        <bl-tab-option v-bind="options" value="progress"
-          >Progress</bl-tab-option
-        >
-        <bl-tab-option v-bind="options" value="tracked">Entries</bl-tab-option>
-      </template>
-
-      <bl-tab value="progress">
-        <template v-if="goal.interval === 'total'">
-          <bl-line-chart
-            :height="200"
-            :items="[
-              {
-                label: 'achieved',
-                color: tailwind.theme.colors.main,
-                values: [
-                  { x: toFullDateCompact('2020-01-01'), y: 0 },
-                  { x: toFullDateCompact('2020-02-01'), y: 11 },
-                  { x: toFullDateCompact('2020-03-01'), y: 16 },
-                  { x: toFullDateCompact('2020-04-01'), y: 18 },
-                  { x: toFullDateCompact('2020-05-01'), y: 41 },
-                  { x: toFullDateCompact('2020-06-01'), y: 41 },
-                  { x: toFullDateCompact('2020-07-01'), y: 41 },
-                ],
-              },
-            ]"
-          />
+    <template #collapsible>
+      <bl-tabs default-value="progress" class="mt-4" full>
+        <template #options="options">
+          <bl-tab-option v-bind="options" value="progress"
+            >Progress</bl-tab-option
+          >
+          <bl-tab-option v-bind="options" value="tracked"
+            >Entries</bl-tab-option
+          >
         </template>
-      </bl-tab>
-      <bl-tab value="tracked">WIP</bl-tab>
-    </bl-tabs>
+
+        <bl-tab value="progress">
+          <template v-if="goal.interval === 'total'">
+            <bl-line-chart
+              :height="200"
+              :items="[
+                {
+                  label: 'achieved',
+                  color: tailwind.theme.colors.main,
+                  values: [
+                    { x: toFullDateCompact('2020-01-01'), y: 0 },
+                    { x: toFullDateCompact('2020-02-01'), y: 11 },
+                    { x: toFullDateCompact('2020-03-01'), y: 16 },
+                    { x: toFullDateCompact('2020-04-01'), y: 18 },
+                    { x: toFullDateCompact('2020-05-01'), y: 41 },
+                    { x: toFullDateCompact('2020-06-01'), y: 41 },
+                    { x: toFullDateCompact('2020-07-01'), y: 41 },
+                  ],
+                },
+              ]"
+            />
+          </template>
+        </bl-tab>
+        <bl-tab value="tracked">WIP</bl-tab>
+      </bl-tabs>
+    </template>
   </bl-tile>
 </template>
 
--- components/tile.vue
+++ components/tile.vue
@@ -1,16 +1,83 @@
+<!-- eslint-disable tailwindcss/no-custom-classname -->
 <template>
-  <div
+  <AccordionRoot
+    default-value="tile"
     class="flex flex-col gap-3 rounded-xl border border-accent bg-white px-8 py-6"
+    type="single"
+    :collapsible="true"
+    :disabled="!$slots['collapsible']"
   >
-    <div
-      v-if="$slots['title'] || $slots['actions']"
-      class="flex justify-between gap-3"
-    >
-      <h5 v-if="$slots['title']"><slot name="title" /></h5>
-      <slot name="actions" />
-    </div>
-    <slot />
-  </div>
+    <AccordionItem value="tile">
+      <AccordionHeader
+        v-if="$slots['title'] || $slots['actions']"
+        as="div"
+        class="flex w-full"
+      >
+        <AccordionTrigger
+          class="accordion-trigger flex flex-1 items-center justify-between gap-3"
+        >
+          <h5 v-if="$slots['title']"><slot name="title" /></h5>
+          <slot name="actions" />
+          <IconChevronDown
+            class="accordion-chevron text-accent-darker transition-transform duration-300 ease-in"
+            :size="ICON_SIZE_SMALL"
+          />
+        </AccordionTrigger>
+      </AccordionHeader>
+      <slot />
+      <AccordionContent>
+        <slot name="collapsible" />
+      </AccordionContent>
+    </AccordionItem>
+  </AccordionRoot>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { IconChevronDown } from '@tabler/icons-vue'
+import {
+  AccordionContent,
+  AccordionHeader,
+  AccordionItem,
+  AccordionRoot,
+  AccordionTrigger,
+} from 'radix-vue'
+</script>
+
+<style scoped>
+.accordion-content {
+  overflow: hidden;
+}
+
+.accordion-content[data-state='open'] {
+  animation: slideDown 300ms ease-out;
+}
+
+.accordion-content[data-state='closed'] {
+  animation: slideUp 300ms ease-out;
+}
+
+.accordion-chevron {
+  transition: transform 300ms;
+}
+.accordion-trigger[data-state='open'] > .accordion-chevron {
+  transform: rotate(180deg);
+}
+
+@keyframes slideDown {
+  from {
+    height: 0;
+  }
+  to {
+    height: var(--radix-accordion-content-height);
+  }
+}
+
+@keyframes slideUp {
+  from {
+    height: var(--radix-accordion-content-height);
+  }
+  to {
+    height: 0;
+  }
+}
+</style>
