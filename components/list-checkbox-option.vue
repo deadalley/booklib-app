c8c58980500bca85926ca4e5e57d5bfd4539f1ce
Jessica Aissa <jessica.aissa@live.com>
2025-07-30 10:01:02 +0200

    fix: 🐛 fix wrong model in list checkbox option

--- components/list-checkbox-option.vue
+++ components/list-checkbox-option.vue
@@ -2,7 +2,7 @@
   <li
     class="border-b border-b-accent px-4 py-2 first:rounded-t-2xl last:rounded-b-2xl last:border-b-0 hover:bg-accent-light"
   >
-    <bl-checkbox v-model="_checked" :checked="checked" v-bind="$attrs">
+    <bl-checkbox v-model="checked" :checked="checked" v-bind="$attrs">
       <slot />
     </bl-checkbox>
   </li>
